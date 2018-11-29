577ddb23cc0303f242cbee9312e8caa372f34936
David Nishikawa <david@squishymedia.com>
2018-11-29 08:57:11 -0800

    add createHistory export

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import * as routerV3 from 'react-router'
+import * as history from 'history'
+
+export const createRouter3History = options => routerV3.useRouterHistory(history.createHistory)(options)
+
 export default routerV3
